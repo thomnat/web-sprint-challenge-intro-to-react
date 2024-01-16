9ae73166a51c3e6111cd7bda36f23ea81920d457
Natalie Thompson <nataliethompson2314@gmail.com>
2024-01-16 12:10:51 -0800

    almost done

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
+
 
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
+
+  const [characters, setCharacters] = useState([]);
+  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [planetsResponse, peopleResponse] = await Promise.all([
+          axios.get(urlPlanets),
+          axios.get(urlPeople)
+        ]);
+
+        const planets = planetsResponse.data;
+        const people = peopleResponse.data;
+
+        const updatedPeople = people.map(person => {
+          const matchingPlanet = planets.find(planet => planet.id === person.homeworld);
+          return {
+            ...person,
+            planet: matchingPlanet
+          };
+        });
+        
+        setCharacters(updatedPeople);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleCardClick = () => {
+    console.log('card clicked!');
+  };
+
+
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
   return (
@@ -13,6 +52,11 @@ function App() {
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {characters.map(character => (
+        <div key={character.id} className='character-card'>
+          <Character data={character} onClick={handleCardClick} />
+        </div>
+      ))}
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
+function Character({ data, onClick }) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const [showHomeworld, setShowHomeworld] = useState(false);
+
+  const handleCardClick = () => {
+    setShowHomeworld(prevState => !prevState);
+  };
+
   return (
-    <div>
+    <div onClick={() => {
+      handleCardClick();
+      onClick();
+    }}>
+      <h3 className='character-name'>{data.name}</h3>
+      {showHomeworld && (
+        <p>
+          Planet: <span>{data.planet ? data.planet.name : 'Unknown'}</span>
+        </p>
+      )}
       {/* Use the same markup with the same attributes as in the mock */}
     </div>
-  )
+  );
 }
 
 export default Character
